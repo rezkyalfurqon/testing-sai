a1e7e884e5f1dba7bf91b760b9fb38f2c4d703ec
fachriemohammad <rezkyalfurqon.ra7@gmail.com>
2022-10-06 13:40:52 +0700

    First Commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<!-- <template>
+  <v-footer dark padless app inset>
+    <v-card class="flex" flat tile color="red" :elevation="10">
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} —
+        <strong>Satuan Audit Internal</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template> -->
---
+++ src/components/Navbar.vue
@@ -0,0 +1,127 @@
+<template>
+  <nav>
+    <v-app-bar color="#b91432" dark app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase ">
+        <span>SAI-</span>
+        <span>Board</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn text v-on="on">
+            <v-icon left>expand_more</v-icon>
+            <v-avatar size="30">
+              <img src="/img1.png" alt="" />
+            </v-avatar>
+          </v-btn>
+        </template>
+        <v-list flat>
+          <!-- <v-list-item
+            v-for="link in links"
+            :key="link.text"
+            router
+            :to="link.route"
+            active-class="border"
+          > -->
+          <v-list-item>
+            <v-list-item-title>Profile</v-list-item-title>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" dark app color="#a19e9e">
+      <v-layout column align-center>
+        <v-flex class="mt-5">
+          <v-avatar size="100">
+            <img src="/img1.png" alt="" />
+          </v-avatar>
+          <p class="white--text subheading mt-1 text-center">Username</p>
+        </v-flex>
+        <v-flex class="mt-4 mb-4">
+          <Popup />
+        </v-flex>
+      </v-layout>
+      <!-- <v-list flat>
+        <v-list-item
+          v-for="link in links"
+          :key="link.text"
+          router
+          :to="link.route"
+          active-class="border"
+        >
+          <v-list-item-action>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list> -->
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+
+      <v-list-group prepend-icon="mdi-account-circle">
+        <template v-slot:activator>
+          <v-list-item-title>Survei Kepuasan Dosen</v-list-item-title>
+        </template>
+
+        <v-list-group sub-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>EDoM</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-group>
+        <v-list-group sub-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>EDWoM</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-group>
+        <v-list-group sub-group>
+          <template v-slot:activator>
+            <v-list-item-content>
+              <v-list-item-title>EDPoM</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-group>
+      </v-list-group>
+    </v-navigation-drawer>
+  </nav>
+</template>
+<script>
+import Popup from "./Popup.vue";
+export default {
+  data: () => ({
+    drawer: true,
+    links: [
+      { icon: "home", text: "Home", route: "/" },
+      {
+        icon: "folder_shared",
+        text: "Survei Kepuasan Dosen",
+        route: "/projects",
+      },
+      { icon: "person", text: "Survei Kepuasan", route: "/team" },
+    ],
+  }),
+  components: {
+    Popup,
+  },
+};
+</script>
+<style scoped>
+.border {
+  border-left: 4px solid #0ba518;
+}
+</style>
